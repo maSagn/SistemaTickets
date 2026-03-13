a21a619a140f4ccd08ed860b56e310a6dc4a00dd
Pokebruno-32 <brunomoal27@hotmail.com>
2026-03-13 16:19:41 -0600

    Creacion de repositorio

---
+++ src/main/java/com/Msanchez/SistemaTickets/SistemaTicketsApplication.java
@@ -0,0 +1,13 @@
+package com.Msanchez.SistemaTickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaTicketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaTicketsApplication.class, args);
+	}
+
+}
